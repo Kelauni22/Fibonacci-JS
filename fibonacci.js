0980afa59db4bb2b813cd15aa3e46d1f7a336f27
Kelauni22 <Kelauni.Cook@gmail.com>
2016-07-24 17:08:32 -0400

    First pass of written program

--- fibonacci.js
+++ fibonacci.js
@@ -1,1 +1,25 @@
 //Fibonacci JavaScript
+
+// Use a for or while loop to print out the first 40 places of the fibonacci series.
+// The series is as follows: 1, 1, 2, 3, 5, 8, 13, 21, 34, 55...
+// Each number is the sum of the two numbers before it
+// Your output can have each number on its own line
+
+
+
+
+var fib_array = [];
+i = 2;
+fib_array[0] = 1;
+fib_array[1] = 1;
+
+while (i < 40){
+	
+	fib_array[i] = fib_array[i - 2] + fib_array[i - 1];
+	i++;
+	
+	
+};
+
+console.log(fib_array);
+
